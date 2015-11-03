4fc1865695088abc9e098bdb9928045b8737d759
Jacob Mziya <jacobmziya@gmail.com>
2015-11-03 15:25:51 +0200

    Added Test Status Table in the seed file.

--- app/database/seeds/TestDataSeeder.php
+++ app/database/seeds/TestDataSeeder.php
@@ -40,7 +40,21 @@ class TestDataSeeder extends DatabaseSeeder
 
         $this->command->info("User Test Category seeded.");
 
-        // ++++++++++++++++++++++++++++++        
+        // ++++++++++++++++++++++++++++++  
+
+        /* Test Status table */
+        $test_statuses = array(
+          array("id" => "1","name" => "not-received","test_phase_id" => "1"),//Pre-Analytical
+          array("id" => "2","name" => "pending","test_phase_id" => "1"),//Pre-Analytical
+          array("id" => "3","name" => "started","test_phase_id" => "2"),//Analytical
+          array("id" => "4","name" => "completed","test_phase_id" => "3"),//Post-Analytical
+          array("id" => "5","name" => "verified","test_phase_id" => "3")//Post-Analytical
+        );
+        foreach ($test_statuses as $test_status)
+        {
+            TestStatus::create($test_status);
+        }
+        $this->command->info('test_statuses seeded');  
         
         /* Measure Types */
         $measureTypes = array(
