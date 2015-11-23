aeb01b09dfaf0c94dc41ffa210bb13ca9593bcf5
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2015-11-23 10:26:02 +0200

    Modified logic for dumping seed data to include specification for database name prompt

--- metadata_table_list.sh
+++ metadata_table_list.sh
@@ -4,6 +4,10 @@ unset username
 
 # clear
 
+echo "Specify database name:"
+
+read database
+
 STR=$'Enter username:\r'
 
 echo "$STR"
@@ -25,7 +29,7 @@ do
     password+="$char"
 done
 
-mysqldump -u $username -p$password iblis --tables facilities wards visit_types visittype_wards test_types test_statuses testtype_specimentypes testtype_measures testtype_organisms test_categories specimen_types specimen_statuses roles organisms organism_drugs measures measure_types measure_ranges drugs permission_role panel_types panels> metadata.sql
+mysqldump -u $username -p$password $database --tables facilities wards visit_types visittype_wards test_types test_statuses testtype_specimentypes testtype_measures testtype_organisms test_categories specimen_types specimen_statuses roles organisms organism_drugs measures measure_types measure_ranges drugs permission_role panel_types panels> metadata.sql
 
 clear
 
