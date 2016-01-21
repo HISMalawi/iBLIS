acd4fe9162b16558f46807558fe5ed72f8fccc22
phundi <kennethkapundi1@gmail.com>
2016-01-21 14:35:15 +0200

    Enabled admin to edit his own associated labsections;

--- app/views/user/edit.blade.php
+++ app/views/user/edit.blade.php
@@ -90,7 +90,7 @@
 							            </div>
 						            </div>
 									<div class="row">
-										@if(Auth::id() != $user->id && Entrust::hasRole(Role::getAdminRole()->name))
+										@if(Entrust::hasRole(Role::getAdminRole()->name))
 										<div class="form-group">
 											{{ Form::label('test_categories', trans('messages.select-lab-section')) }}
 											<div class="form-pane panel panel-default">
