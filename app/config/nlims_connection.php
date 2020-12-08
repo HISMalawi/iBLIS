709fd63fdd4090c8f9434ace8ed31ad4332869d5
GiboProgrammer <gmalolo@pedaids.org>
2020-12-08 17:02:35 +0200

    changed app name

--- app/config/nlims_connection.php
+++ app/config/nlims_connection.php
@@ -7,7 +7,7 @@ return array(
     'nlims_controller_ip' => 'http://192.168.43.50:3009',
     'nlims_default_password' => 'knock_knock',
     'nlims_default_username' => 'admin',
-    'app_name' => 'iblis_',
+    'app_name' => 'iblis_1',
     'partner_name' => 'baobab health trust',
     'nlims_custome_password' => 'zomba_iblis_lims',
     'nlims_custome_username' => 'zomba_iblis_lims',
