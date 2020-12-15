da15d81ec5729c4a6cb931abc69be5a3b2538cb0
GiboProgrammer <gmalolo@pedaids.org>
2020-12-15 13:34:50 +0200

    added new workflow in interacting with nlims

---
+++ app/models/UnsyncOrder.php
@@ -0,0 +1,16 @@
+<?php
+
+class UnsyncOrder extends Eloquent
+{
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'unsync_orders';
+
+	public $timestamps = true;
+
+	
+
+}
