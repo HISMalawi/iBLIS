b9ace4d37f1d157ad0e726d94a5b912c05e0cff7
GiboProgrammer <gmalolo@pedaids.org>
2021-09-06 14:03:29 +0200

    added default settings for nlims

--- app/config/nlims_connection.php
+++ app/config/nlims_connection.php
@@ -4,13 +4,13 @@
 return array(
 
     'nlims_controller' => true,
-    'nlims_controller_ip' => 'http://192.168.100.200:3009',
+    'nlims_controller_ip' => 'http://localhost:3009',
     'nlims_default_password' => 'knock_knock',
     'nlims_default_username' => 'admin',
     'app_name' => 'iblis_1',
-    'partner_name' => 'baobab health trust',
-    'nlims_custome_password' => 'area18_iblis_lims',
-    'nlims_custome_username' => 'area18_iblis_lims',
+    'partner_name' => 'EGPAF',
+    'nlims_custome_password' => 'iblis_lims',
+    'nlims_custome_username' => 'iblis_lims',
     'nlims_api_version' => 'v1', 
     'district' => 'Lilongwe'
 
