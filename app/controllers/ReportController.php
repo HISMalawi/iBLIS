63f0f040cfb4447cd85c439d77282cbf1e0a2661
Blessings Chidambe <bchidambe@gmail.com>
2022-09-20 09:22:48 +0200

    Total Positive

--- app/controllers/ReportController.php
+++ app/controllers/ReportController.php
@@ -1696,6 +1696,7 @@ P1
 
 		$indicators = array(
 				"Total malaria microscopy tests done",
+				"Total positive malaria microscopy tests done",
 				"Malaria microscopy in <= 5yrs (by species)?",
 				"Positive malaria slides in < 5yrs",
 				"Malaria microscopy in unknown age",
