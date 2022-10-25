a5f735759308096b3db2b0e7f1297024be4bea71
Blessings Chidambe <bchidambe@gmail.com>
2022-10-25 11:08:49 +0200

    space remove

--- app/views/test/viralLoadSampleEntry.blade.php
+++ app/views/test/viralLoadSampleEntry.blade.php
@@ -31,7 +31,7 @@
                                 <div class="card">
                                     <div class="card-body">
                                         <div class="row">
-                                            <div class="form-group col-lg-6">
+                                            <div class="form-group col-lg-6 ec">
                                                 <label class="mt-2" for="small-barcode-one">Scan Barcode : &nbsp; </label>
                                             <input  type="text" class="form-control bar-item" name="small-barcode" id="small_barcode">
     
