822ab0ffbf36cc893ed66dac24ffb53a4bece5c4
GiboProgrammer <gmalolo@pedaids.org>
2023-02-17 23:42:32 +0200

    added function to handle viral load results upload api

--- app/controllers/InterfacerController.php
+++ app/controllers/InterfacerController.php
@@ -23,6 +23,119 @@ class InterfacerController extends \BaseController
 
         return json_encode($catalog);
     }
+    
+    public function uploadViralLoadResult(){
+
+        $username = null;
+        $password = null;
+
+        // mod_php
+        if (isset($_SERVER['PHP_AUTH_USER'])) {
+            $username = $_SERVER['PHP_AUTH_USER'];
+            $password = $_SERVER['PHP_AUTH_PW'];
+
+        // most other servers
+        } elseif (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+
+            if (strpos(strtolower($_SERVER['HTTP_AUTHORIZATION']), 'basic') === 0)
+                list($username, $password) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+
+        }
+
+        if (is_null($username)) {
+
+            header('WWW-Authenticate: Basic realm="My Realm"');
+            header('HTTP/1.0 401 Unauthorized');
+            echo 'Text to send if user hits Cancel button';
+
+            die();
+
+        } else {
+
+            $credentials = array(
+                "username" => $username,
+                "password" => $password
+            );
+
+            if (!Auth::attempt($credentials)) {
+
+                header('WWW-Authenticate: Basic realm="My Realm"');
+                header('HTTP/1.0 401 Unauthorized');
+                echo 'Text to send if user hits Cancel button';
+
+                die();
+
+            }
+        }
+
+
+
+
+
+        $json = array();
+
+        $base = realpath(".");
+
+        if (!file_exists($base . "/data")) {
+            mkdir($base . "/data", 0777, true);
+        }
+
+        $specimen_id = strtoupper(trim($_REQUEST["specimen_id"]));
+        $code = "/^".Config::get('kblis.facility-code')."\d+$/";
+
+        if (preg_match("/^\d+$/", $specimen_id)){
+            $specimen_id = Config::get('kblis.facility-code').$specimen_id;
+        }else if(preg_match($code, $specimen_id)){
+            //The specimen_id is already an accession number; no modifications required
+        }else{
+            //This must be a tracking number, get associated accession number
+            if (!preg_match("/^X/", $specimen_id)){
+                $specimen_id = "X".$specimen_id;
+            }
+
+            try {
+                $s_id = DB::table("specimens")->where("tracking_number", $specimen_id)->first()->accession_number;
+                if($s_id){
+                    $specimen_id = $s_id;
+                }
+            }catch (Exception $e){
+                //Fetching will fail
+            }
+        }
+
+        if (preg_match("/INVALID/", $specimen_id)){
+            return "-1";
+        }
+
+
+        $machine_name = "";
+        if(!empty($_REQUEST["machine_name"])){
+                $machine_name = $_REQUEST["machine_name"];
+        }
+    
+            $measure_id = $_REQUEST["measure_id"];
+            $result = $_REQUEST["result"];
+            $remote_ip = '';    // $_SERVER["REMOTE_ADDR"] . "/";
+    
+         
+            if(isset($specimen_id) && isset($measure_id) && isset($result)) {
+    
+                if(!isset($json[$specimen_id])) {
+    
+                    $json[$specimen_id] = array(
+                        $measure_id => $result
+                    );
+    
+                } else {
+    
+                    $json[$specimen_id][$measure_id] = $result;
+    
+                }
+            }
+
+
+
+    }
 
     public function receiveLabRequest()
     {
