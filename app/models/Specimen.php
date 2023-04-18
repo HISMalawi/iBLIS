27da9cd4744b3971139ddcec6f03a5b561ba9358
Hopson Gausi <hopgausi20@gmail.com>
2023-04-18 06:38:25 +0200

    Config to either use default accession # or not

--- app/models/Specimen.php
+++ app/models/Specimen.php
@@ -244,17 +244,24 @@ class Specimen extends Eloquent
 
 	public static function assignAccessionNumber(){
 		# Generate the next accession number for specimen registration
-
 		$max_acc_num = null;
 		$return_value = null;
-		$sentinel = 99999999;
+
+		if (Config::get('app.useDefaultAccessionNumberLength')) {
+			$sentinel = 99999999;
+			$total_length = 8;
+		}
+		else {
+			$sentinel = 999999;
+			$total_length = 6;
+		}
 
 		$code = Config::get('kblis.facility-code');
 
 		$record = DB::select("SELECT * FROM specimens  WHERE accession_number IS NOT NULL ORDER BY id DESC LIMIT 1");
 
 		if(COUNT($record) > 0){
-			$max_acc_num = (int)substr($record[0]->accession_number, -8);
+			$max_acc_num = (int)substr($record[0]->accession_number, -$total_length);
 			if ($max_acc_num < $sentinel){
 				$max_acc_num += 1;
 			}else{
@@ -264,7 +271,7 @@ class Specimen extends Eloquent
 			$max_acc_num = 1;
 		}
 
-		$max_acc_num = str_pad($max_acc_num, 8, '0', STR_PAD_LEFT);
+		$max_acc_num = str_pad($max_acc_num, $total_length, '0', STR_PAD_LEFT);
 		$year = date('y');
 		$return_value = $code.$year.$max_acc_num;
 		return $return_value;
